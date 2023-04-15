8945eeb228270b445ff649468e35d4d286dd0fe4
Dzakiy Dzakwan <dzakcart@gmail.com>
2023-04-15 22:33:13 +0700

    style: Layout Dashboard Admin

--- resources/views/master/main.blade.php
+++ resources/views/master/main.blade.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @yield('title')
-    
+
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main/app-dark.css') }}">
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.png') }}" type="image/png">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo/350x75.png') }}" type="image/png">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo/logo.png') }}" type="image/png">
     @yield('style')
 </head>
 
