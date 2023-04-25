1cb1c8f62ce273f1ad0d54d5a17f8dda3a744b1e
Dzakiy Dzakwan <dzakcart@gmail.com>
2023-04-25 23:25:24 +0700

    Dzakiy_feature : User,Utensill,Ingredient,Category

---
+++ resources/views/utensill.blade.php
@@ -0,0 +1,87 @@
+@extends('master.main')
+
+@section('title')
+    <title>Utensill Page</title>
+@endsection
+
+@section('style')
+    <link rel="stylesheet" href="{{ asset('assets/css/shared/iconly.css') }}">
+    <link rel="stylesheet" href="assets/extensions/datatables.net-bs5/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="assets/css/pages/datatables.css">
+@endsection
+
+@section('content')
+    <div class="page-heading">
+        <h3>Utensill Page</h3>
+    </div>
+    <div class="page-content">
+        <section class="row">
+            <div class="col-12 col-lg-12">
+                <div class="row">
+                    <div class="col-6 col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-4 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                        <div class="stats-icon purple mb-2">
+                                            <i class="iconly-boldShow"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                        <h6 class="text-muted font-semibold">Total Utensill</h6>
+                                        <h6 class="font-extrabold mb-0">112.000</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Utensill Table</h4>
+                            </div>
+                            <div class="card-body">
+                                <table class="table" id="table1">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama</th>
+                                            <th>Bahan</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Panci</td>
+                                            <td>Alumunium</td>
+                                            <td>
+                                                <button class="btn btn-small btn-info">
+                                                    <i class="bi bi-eye"></i>
+                                                </button>
+                                                <button class="btn btn-small btn-warning">
+                                                    <i class="bi bi-pencil"></i>
+                                                </button>
+                                                <button class="btn btn-small btn-danger">
+                                                    <i class="bi bi-trash3"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+@section('script')
+    <script src="assets/extensions/jquery/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/v/bs5/dt-1.12.1/datatables.min.js"></script>
+    <script src="assets/js/pages/datatables.js"></script>
+@endsection
